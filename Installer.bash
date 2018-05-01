dad1cc899d12730b8dcbccba581765082642dc55
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2018-05-01 10:44:49 +0800

    Refactor installer
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- Installer.bash
+++ Installer.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-#shellcheck disable=SC2034
+# shellcheck disable=SC2034
+
+# Install the product into the system
+# 林博仁 © 2018
 
 ## Makes debuggers' life easier - Unofficial Bash Strict Mode
 ## BASHDOC: Shell Builtin Commands - Modifying Shell Behavior - The Set Builtin
@@ -8,10 +11,50 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
+## Runtime Dependencies Checking
+declare\
+	runtime_dependency_checking_result=still-pass\
+	required_software
+
+for required_command in \
+	basename\
+	dirname\
+	install\
+	realpath\
+	rm; do
+	if ! command -v "${required_command}" &>/dev/null; then
+		runtime_dependency_checking_result=fail
+
+		case "${required_command}" in
+			basename\
+			|dirname\
+			|install\
+			|realpath\
+			|rm)
+				required_software='GNU Coreutils'
+				;;
+			*)
+				required_software="${required_command}"
+				;;
+		esac
+
+		printf --\
+			'Error: This program requires "%s" to be installed and its executables in the executable searching paths.\n'\
+			"${required_software}" 1>&2
+		unset required_software
+	fi
+done; unset required_command required_software
+
+if [ "${runtime_dependency_checking_result}" = fail ]; then
+	printf --\
+		'Error: Runtime dependency checking fail, the progrom cannot continue.\n' 1>&2
+	exit 1
+fi; unset runtime_dependency_checking_result
+
 ## Non-overridable Primitive Variables
 ## BASHDOC: Shell Variables » Bash Variables
 ## BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters
-if [ -v "BASH_SOURCE[0]" ]; then
+if [ -v 'BASH_SOURCE[0]' ]; then
 	RUNTIME_EXECUTABLE_PATH="$(realpath --strip "${BASH_SOURCE[0]}")"
 	RUNTIME_EXECUTABLE_FILENAME="$(basename "${RUNTIME_EXECUTABLE_PATH}")"
 	RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
@@ -23,19 +66,18 @@ if [ -v "BASH_SOURCE[0]" ]; then
 		RUNTIME_EXECUTABLE_PATHABSOLUTE\
 		RUNTIME_COMMANDLINE_BASECOMMAND
 fi
-declare -ar RUNTIME_COMMANDLINE_PARAMETERS=("${@}")
-
-declare -i global_just_uninstall="1"
+declare -ar RUNTIME_COMMANDLINE_ARGUMENTS=("${@}")
 
 ## init function: entrypoint of main program
 ## This function is called near the end of the file,
 ## with the script's command-line parameters as arguments
 init(){
-	
-	local install_directory
+	local flag_uninstall=false
+	local install_directory_xdg
 
-	if ! process_commandline_parameters; then
-		printf\
+	if ! process_commandline_arguments\
+			flag_uninstall; then
+		printf --\
 			'Error: %s: Invalid command-line parameters.\n'\
 			"${FUNCNAME[0]}"\
 			1>&2
@@ -43,79 +85,147 @@ init(){
 		exit 1
 	fi
 
-	if ! determine_install_directory install_directory; then
-		printf '錯誤：無法判斷安裝目錄。安裝程式無法繼續運行\n' 1>&2
-		exit "${COMMON_RESULT_FAILURE}"
+	if ! determine_install_directory\
+			install_directory_xdg; then
+		printf -- \
+			'Error: Unable to determine install directory, installer cannot continue.\n' \
+			1>&2
+		exit 1
 	else
-		printf '將會安裝到：%s\n' "${install_directory}"
+		printf -- \
+			'Will be installed to: %s\n' \
+			"${install_directory_xdg}"
 		printf '\n'
 	fi
 
-	remove_old_installation
-	if [ "${global_just_uninstall}" -eq "0" ]; then
-		printf '已解除安裝軟體\n'
+	remove_old_installation\
+		"${install_directory_xdg}"
+	if [ "${flag_uninstall}" = true ]; then
+		printf -- \
+				'Software uninstalled successfully.\n'
 		exit 0
 	fi
 
-	printf '正在安裝範本檔案……\n'
-	cp\
-		--force\
-		--verbose\
-		"${RUNTIME_EXECUTABLE_DIRECTORY}"/template.travis.yml\
-		"${XDG_TEMPLATES_DIR}/travis.yml"
+	printf -- \
+		'Installing template files...\n'
+	install \
+		--verbose \
+		"${RUNTIME_EXECUTABLE_DIRECTORY}/template.travis.yml" \
+		"${install_directory_xdg}/travis.yml"
 	printf '\n' # Seperate output from different operations
 
-	while :; do
-		printf '請問是否安裝 KDE 所需的範本設定（警告：會造成 GNOME Files 等應用軟體中出現非預期的範本項目）(y/N)？'
-		declare answer
+	while true; do
+		printf -- \
+			'Do you want to install files to enable KDE support(y/N)?'
 		read -r answer
 
 		if [ -z "${answer}" ]; then
 			break
 		else
 			# lowercasewize
-			answer="${answer,,?}"
+			answer="$(
+				printf -- \
+					'%s'\
+					"${answer}"\
+					| tr '[:upper:]' '[:lower:]'\
+			)"
 
-			if [ "${answer}" != 'n' ] && [ "${answer}" != 'y' ]; then
+			if [ "${answer}" != n ] && [ "${answer}" != y ]; then
 				# wrong format, re-ask
 				continue
-			elif [ "${answer}" == 'n' ]; then
+			elif [ "${answer}" == n ]; then
 				break
 			else
-				printf '正在設定適用於 KDE 的範本……\n'
-				cp\
-					--force\
-					--verbose\
-					'Template Setup for KDE/'*.desktop\
-					"${XDG_TEMPLATES_DIR}"
+				printf 'Configuring templates for KDE...\n'
+				install \
+					--verbose \
+					"${RUNTIME_EXECUTABLE_DIRECTORY}/template.travis.yml" \
+					"${HOME}/.local/share/templates/travis.yml"
+				install \
+					--verbose \
+					"${RUNTIME_EXECUTABLE_DIRECTORY}/Template Setup for KDE"/*.desktop \
+					"${HOME}/.local/share/templates"
 				break
 			fi
 		fi
 	done; unset answer
 
-	printf '已完成安裝。\n'
+	printf 'Installation completed.\n'
+
 
 	exit 0
 }; declare -fr init
 
-## Attempt to remove old installation files
-remove_old_installation(){
-	printf '正在清除過去安裝範本（如果有的話）……\n'
-	rm\
-		--verbose\
-		--force\
-		"${XDG_TEMPLATES_DIR}"/.travis.yml\
-		"${XDG_TEMPLATES_DIR}"/*travis.yml\
-		|| true
-	printf '完成\n'
+print_help(){
+	printf '# %s #\n' "${RUNTIME_EXECUTABLE_NAME}"
+	printf 'This program installs the templates into the system to make it accessible.\n\n'
 
-	printf '\n' # Additional blank line for separating output
+	printf '## Command-line Options ##\n'
+	printf '### --help / -h ###\n'
+	printf 'Print this message\n\n'
+
+	printf '### --uninstall / -u ###\n'
+	printf 'Instead of installing, attempt to remove previously installed product\n\n'
+
+	printf '### --debug / -d ###\n'
+	printf 'Enable debug mode\n\n'
+
 	return 0
-}
-readonly -f remove_old_installation
+}; declare -fr print_help;
+
+process_commandline_arguments() {
+	local -n flag_uninstall_ref="${1}"
+
+	if [ "${#RUNTIME_COMMANDLINE_ARGUMENTS[@]}" -eq 0 ]; then
+		return 0
+	fi
+
+	# modifyable parameters for parsing by consuming
+	local -a parameters=("${RUNTIME_COMMANDLINE_ARGUMENTS[@]}")
+
+	# Normally we won't want debug traces to appear during parameter parsing, so we  add this flag and defer it activation till returning(Y: Do debug)
+	local enable_debug=N
+
+	while true; do
+		if [ "${#parameters[@]}" -eq 0 ]; then
+			break
+		else
+			case "${parameters[0]}" in
+				--help\
+				|-h)
+					print_help;
+					exit 0
+					;;
+				--uninstall\
+				|-u)
+					flag_uninstall_ref=true
+					;;
+				--debug\
+				|-d)
+					enable_debug=Y
+					;;
+				*)
+					printf 'ERROR: Unknown command-line argument "%s"\n' "${parameters[0]}" >&2
+					return 1
+					;;
+			esac
+			# shift array by 1 = unset 1st then repack
+			unset 'parameters[0]'
+			if [ "${#parameters[@]}" -ne 0 ]; then
+				parameters=("${parameters[@]}")
+			fi
+		fi
+	done
+
+	if [ "${enable_debug}" = Y ]; then
+		trap 'trap_return "${FUNCNAME[0]}"' RETURN
+		set -o xtrace
+	fi
+	return 0
+}; declare -fr process_commandline_arguments
 
 determine_install_directory(){
-	local -n install_directory_ref="$1"
+	local -n install_directory_xdg_ref="${1}"; shift
 
 	# For $XDG_TEMPLATES_DIR
 	if [ -f "${HOME}"/.config/user-dirs.dirs ];then
@@ -124,20 +234,43 @@ determine_install_directory(){
 		source "${HOME}"/.config/user-dirs.dirs
 
 		if [ -v XDG_TEMPLATES_DIR ]; then
-			install_directory_ref="${XDG_TEMPLATES_DIR}"
+			install_directory_xdg_ref="${XDG_TEMPLATES_DIR}"
 			return 0
 		fi
 	fi
 
-	printf '%s - 警告 - 安裝程式找不到 user-dirs 設定，汰退到預設目錄\n' "${FUNCNAME[0]}"
+	printf -- \
+		"%s: Warning: Installer can't locate user-dirs configuration, will fallback to unlocalized directories\\n" \
+		"${FUNCNAME[0]}" \
+		1>&2
 
 	if [ ! -d "${HOME}"/Templates ]; then
-		return "${COMMON_RESULT_FAILURE}"
+		return 1
 	else
-		install_directory_ref="${HOME}"/Templates
+		install_directory_xdg_ref="${HOME}"/Templates
 	fi
 
-}; readonly -f determine_install_directory
+}; declare -fr determine_install_directory
+
+## Attempt to remove old installation files
+remove_old_installation(){
+	local install_directory_xdg="${1}"; shift 1
+
+	printf 'Removing previously installed templates(if available)...\n'
+	rm\
+		--verbose\
+		--force\
+		"${install_directory_xdg}"/*travis.yml
+	rm\
+		--verbose\
+		--force\
+		"${HOME}/.local/share/templates"/*travis.yml\
+		"${HOME}/.local/share/templates"/travis.yml.desktop
+	printf 'Finished.\n'
+
+	printf '\n' # Additional blank line for separating output
+	return 0
+}; declare -fr remove_old_installation
 
 ## Traps: Functions that are triggered when certain condition occurred
 ## Shell Builtin Commands » Bourne Shell Builtins » trap
@@ -157,64 +290,15 @@ trap_return(){
 }; declare -fr trap_return
 
 trap_interrupt(){
-	printf 'Recieved SIGINT, script is interrupted.\n' 1>&2
-	return 0
+	printf '\n' # Separate previous output
+	printf 'Recieved SIGINT, script is interrupted.' 1>&2
+	return 1
 }; declare -fr trap_interrupt; trap trap_interrupt INT
-
-print_help(){
-	printf 'Currently no help messages are available for this program\n' 1>&2
-	return 0
-}; declare -fr print_help;
-
-process_commandline_parameters() {
-	if [ "${#RUNTIME_COMMANDLINE_PARAMETERS[@]}" -eq 0 ]; then
-		return 0
-	fi
-
-	# modifyable parameters for parsing by consuming
-	local -a parameters=("${RUNTIME_COMMANDLINE_PARAMETERS[@]}")
-
-	# Normally we won't want debug traces to appear during parameter parsing, so we  add this flag and defer it activation till returning(Y: Do debug)
-	local enable_debug=N
-
-	while true; do
-		if [ "${#parameters[@]}" -eq 0 ]; then
-			break
-		else
-			case "${parameters[0]}" in
-				"--help"\
-				|"-h")
-					print_help;
-					exit 0
-					;;
-				"--debug"\
-				|"-d")
-					enable_debug="Y"
-					;;
-				*)
-					printf 'ERROR: Unknown command-line argument "%s"\n' "${parameters[0]}" >&2
-					return 1
-					;;
-			esac
-			# shift array by 1 = unset 1st then repack
-			unset "parameters[0]"
-			if [ "${#parameters[@]}" -ne 0 ]; then
-				parameters=("${parameters[@]}")
-			fi
-		fi
-	done
-
-	if [ "${enable_debug}" = "Y" ]; then
-		trap 'trap_return "${FUNCNAME[0]}"' RETURN
-		set -o xtrace
-	fi
-	return 0
-}; declare -fr process_commandline_parameters;
 
 init "${@}"
 
 ## This script is based on the GNU Bash Shell Script Template project
 ## https://github.com/Lin-Buo-Ren/GNU-Bash-Shell-Script-Template
 ## and is based on the following version:
-declare -r META_BASED_ON_GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v1.26.0-32-g317af27-dirty"
+## GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v3.0.0-4-g5de1348"
 ## You may rebase your script to incorporate new features and fixes from the template
